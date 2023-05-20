a8a871afc29177e10b4826f528fd9c29ff60a233
Seevakan1997 <seevakan1@gmail.com>
2023-05-20 19:25:02 +0530

    updated

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
-
-
+const multer = require('multer');
+const path = require("path");
 
 dotenv.config();
 mongoose.set("strictQuery", false);
@@ -22,10 +22,30 @@ mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true,
         console.error(err);
       });
 
+app.use("/images", express.static(path.join(__dirname,"public/images")));
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination:(req,file,cb)=>{
+    cb(null, "public/images");
+  },
+  filename:(req,file,cb)=>{
+    cb(null,req.body.name);
+  }
+})
+
+const uplaod = multer({storage});
+app.post("/api/upload",uplaod.single("file"),(req,res)=>{
+  try{
+    return res.status(200).json("File Uploaded Successfully");
+  }catch(err){
+    console.log(err);
+  }
+});
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
